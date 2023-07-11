b1b9c804b560ae5c5eca29621bc4c5274dbf0bae
Pradnyesh Bhadlkar <pradnyeshbhadlkar7247@gmail.comm>
2023-07-11 15:41:22 +0530

    first commit

--- src/Register.jsx
+++ src/Register.jsx
@@ -6,14 +6,14 @@ const Register = () => {
         <><h2>Sign Up </h2>
       <form className="register-form">
       <label for="First Name">First Name</label>
-        <input type=" First Name" placeholder=" First Name" id="First Name" name="First Name"/>
+        <input type=" First Name" placeholder=" First Name" id="First Name" name="First Name" required={true} />
         <label for="email">LastName</label>
-        <input type="Last Name" placeholder="Last Name" id="Last Name" name="Last Name"/>
+        <input type="Last Name" placeholder="Last Name" id="Last Name" name="Last Name" required={true}/>
         <label for="email">email</label>
-        <input type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+        <input type="email" placeholder="youremail@gmail.com" id="email" name="email" required={true}/>
         <label for="password">Password</label>
-        <input type="password" placeholder="********" id="password" name="password"/>
-        <button>Sign Up</button>
+        <input type="password" placeholder="********" id="password" name="password" required={true}/>
+        <button type="submit">Sign Up</button>
       </form>
       <button className="link-button">Don't have an account? Register here</button>
       </>
